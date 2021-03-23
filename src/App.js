459589fab8750b9e0557fdbe37bd9ab5a3e303ff
vincentservio <vincentservio@gmail.com>
2021-03-23 17:59:40 -0400

    .v3

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import Form from './Form';
-import './App.css';
-import React, {Component} from 'react'
-import  { connect } from  'react-redux'
-import { getTakeouts } from './actions/takeout'
-class App extends Component{
-
-  componentDidMount(){
-    this.props.getTakeouts()
+import Form from "./Form";
+import "./App.css";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {getTakeouts} from "./actions/takeout";
+class App extends Component {
+  componentDidMount() {
+    debugger;
+    this.props.getTakeouts();
   }
-  render(){   
- 
-  const takeouts = this.props.takeouts.map((takeout, i) => <li key={i}>{takeout.name}</li>)
+  render() {
+    const takeouts = this.props.takeouts.map((takeout, i) => (
+      <li key={i}>{takeout.name}</li>
+    ));
     return (
       <div className="App">
-          <header className="App-header">
-            <h1>hello</h1>
+        <header className="App-header">
+          <h1>hello</h1>
 
-            <Form/>
-            <ul>
-              {this.props.loading ? <h3>Loading.....</h3> : takeouts}
-              <br/>
-            </ul>
-          </header>
-        </div>
-    )
-  } 
-  
-}
-
-const mapStateToProps = state => {
-  return {
-    takeouts: state.takeoutReducer.takeout,
-    loading: state.takeoutReducer.loading
-
+          <Form />
+          <ul>
+            {this.props.loading ? <h3>Loading.....</h3> : takeouts}
+            <br />
+          </ul>
+        </header>
+      </div>
+    );
   }
 }
-export default connect(mapStateToProps, getTakeouts )(App);
+
+const mapStateToProps = (state) => {
+  return {
+    takeouts: state.takeoutReducer.takeout,
+    loading: state.takeoutReducer.loading,
+  };
+};
+export default connect(mapStateToProps, getTakeouts)(App);
